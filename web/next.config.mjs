03b54b2396901a87a856780e0fbb0ce09aeb3822
SAMKIEL <samuelezekiel488@gmail.com>
2026-04-03 04:19:05 +0100

    feat: configure Next.js to support ffmpeg server-side dependencies

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        serverComponentsExternalPackages: ['@ffmpeg-installer/ffmpeg', 'fluent-ffmpeg']
+    }
+};
 
 export default nextConfig;
